efb08c2cebd47de5d2456be17c8735398499972e
root <yangxu@qq.com>
2020-08-23 21:27:00 +0800

    第一次提交GitTest1项目

---
+++ FirstGitTest/GitTest1/src/com/ithei/hello.java
@@ -0,0 +1,15 @@
+package com.ithei;
+
+/**
+ * @PackageName: com.ithei
+ * @ClassName: hello
+ * @Description: TODO
+ * @Author: yangxu
+ * @DateTime: 2020/8/23 21:12
+ * @Version 1.0
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("第一次玩Git");
+    }
+}
