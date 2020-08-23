616923d482a7796f5baa415d71c70d91dcd82521
root <yangxu@qq.com>
2020-08-23 21:31:33 +0800

    第2次提交GitTest1项目

--- FirstGitTest/GitTest1/src/com/ithei/hello.java
+++ FirstGitTest/GitTest1/src/com/ithei/hello.java
@@ -10,6 +10,12 @@ package com.ithei;
  */
 public class hello {
     public static void main(String[] args) {
+        System.out.println("第0次玩Git");
         System.out.println("第一次玩Git");
+        System.out.println("第2次玩Git");
+        System.out.println("第一次玩Git");
+    }
+    public static void m2() {
+        System.out.println("第2次玩Git");
     }
 }
