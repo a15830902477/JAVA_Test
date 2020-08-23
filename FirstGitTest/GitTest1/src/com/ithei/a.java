740986241b49de669320b433d396e78f561237e4
root <yangxu@qq.com>
2020-08-23 22:00:50 +0800

    提交a

---
+++ FirstGitTest/GitTest1/src/com/ithei/a.java
@@ -0,0 +1,15 @@
+package com.ithei;
+
+/**
+ * @PackageName: com.ithei
+ * @ClassName: a
+ * @Description: TODO
+ * @Author: yangxu
+ * @DateTime: 2020/8/23 21:59
+ * @Version 1.0
+ */
+public class a {
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
