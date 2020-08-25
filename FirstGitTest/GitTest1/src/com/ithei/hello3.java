d199e7997a8b24120161a54ab957477d11c7c0d3
root <yangxu@qq.com>
2020-08-25 18:40:58 +0800

    提交3_1

---
+++ FirstGitTest/GitTest1/src/com/ithei/hello3.java
@@ -0,0 +1,16 @@
+package com.ithei;
+
+/**
+ * @PackageName: com.ithei
+ * @ClassName: hello3
+ * @Description: TODO
+ * @Author: yangxu
+ * @DateTime: 2020/8/25 18:40
+ * @Version 1.0
+ */
+public class hello3 {
+    public static void main(String[] args) {
+        System.out.println(1);
+
+    }
+}
