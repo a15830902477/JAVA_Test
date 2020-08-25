7e9b87d5e60782877bfc5e06c2b555c903ecf6f3
root <yangxu@qq.com>
2020-08-25 19:00:24 +0800

    Revert "第一次提交GitTest1项目"
    This reverts commit efb08c2cebd47de5d2456be17c8735398499972e.
    
    # Conflicts:
    #	FirstGitTest/GitTest1/GitTest1.iml
    #	FirstGitTest/GitTest1/src/com/ithei/hello.java

--- FirstGitTest/GitTest1/src/com/ithei/hello.java
+++ FirstGitTest/GitTest1/src/com/ithei/hello.java
@@ -10,18 +10,6 @@ package com.ithei;
  */
 public class hello {
     public static void main(String[] args) {
-        System.out.println("第0次玩Git");
         System.out.println("第一次玩Git");
-        System.out.println("第2次玩Git");
-        System.out.println("第一次玩Git");
-    }
-    public static void m2() {
-        System.out.println("第2次玩Git");
-        System.out.println("第2次玩Git");
-        System.out.println("第2次玩Git");
-        System.out.println("第2次玩Git");
-    }
-    public void m(){
-
     }
 }
