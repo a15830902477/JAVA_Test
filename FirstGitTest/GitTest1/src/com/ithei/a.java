9420075327865b0354c49304efc003b6f7a9e221
root <yangxu@qq.com>
2020-08-25 19:09:25 +0800

    Revert "第2次提交GitTest1项目"
    This reverts commit 616923d4

--- FirstGitTest/GitTest1/src/com/ithei/a.java
+++ FirstGitTest/GitTest1/src/com/ithei/a.java
@@ -11,5 +11,9 @@ package com.ithei;
 public class a {
     public static void main(String[] args) {
         System.out.println(111);
+        System.out.println(111);
+        System.out.println(111);
+        System.out.println(111);
+        System.out.println(111);
     }
 }
